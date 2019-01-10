008d59b726d2ba911d85d372d192b844fd8423ca
Scott Riley <hugs@scott.is>
2019-01-10 15:07:35 +0000

    ESLint fix

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import {Fragment} from 'react'
+import Head from 'next/head'
+
 import GlobalStyles from '../components/GlobalStyles'
 
 export default () => (
   <Fragment>
+    <Head>
+      <title>Smash The Patriarchy</title>
+    </Head>
     <div>Smash the patriarchy</div>
     <GlobalStyles />
   </Fragment>
